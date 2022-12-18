7cd921bb82f2372a33b1180949647221ad3e10e6
SergioAJS <SergFanMadrid@gmail.com>
2022-12-18 22:24:39 +0500

    fix: fix styles of Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,12 +8,12 @@ function Card(props) {
       </div>
       <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
       <h5>{props.title}</h5>
-      <div className="description">
+      <div className={styles.description}>
         <div>
           <span>Цена:</span>
           <b>{props.price} руб.</b>
         </div>
-        <button className="button" onClick={props.onClick}>
+        <button className={styles.button} onClick={props.onClick}>
           <img width={11} height={11} src="img/plus.svg" alt="Plus" />
         </button>
       </div>
