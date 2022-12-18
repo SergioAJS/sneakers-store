8e7990e97922d09b4a8f96c8d6b08ebdc0e3e5c4
SergioAJS <SergFanMadrid@gmail.com>
2022-12-18 22:05:42 +0500

    feat: props and style modules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import Header from "./components/Header";
 import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    name: "Мужские кроссовки Nike Blazer",
+    price: 12999,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    name: "Мужские кроссовки Reebok",
+    price: 13300,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    name: "Мужские кроссовки Nike Air",
+    price: 12699,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper">
@@ -18,10 +36,15 @@ function App() {
         </div>
 
         <div className="cards">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj, index) => (
+            <Card
+              title={obj.name}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+              key={index}
+            />
+          ))}
         </div>
       </div>
     </div>
